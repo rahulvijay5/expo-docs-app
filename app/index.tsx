9f4173a99df91dadcd9b12190668aa1dcf6dfdfa
Rahul Vijay <111470767+rahulvijay5@users.noreply.github.com>
2025-01-01 20:02:51 +0530

    initial expo setup, with nativewind installed

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Text className='bg-orange-500 p-4 font-bold mt-2 rounded-lg '>Native Wind is Started?</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
